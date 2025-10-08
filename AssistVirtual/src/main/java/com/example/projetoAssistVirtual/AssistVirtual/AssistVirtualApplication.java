76fe07e8aaa33c0a764e0496425e8f776729cb19
GlauberViniciusCB <g_viniciuscb@outlook.com>
2025-10-08 20:06:59 -0300

    feat: Iniciando A Estrutura Do Projeto

---
+++ AssistVirtual/src/main/java/com/example/projetoAssistVirtual/AssistVirtual/AssistVirtualApplication.java
@@ -0,0 +1,14 @@
+package com.example.projetoAssistVirtual.AssistVirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistVirtualApplication.class, args);
+		
+	}
+
+}
---
+++ AssistVirtual/src/test/java/com/example/projetoAssistVirtual/AssistVirtual/AssistVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoAssistVirtual.AssistVirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
